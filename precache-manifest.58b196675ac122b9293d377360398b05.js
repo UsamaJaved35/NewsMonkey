78ee5579b186a12733117353046819c513cdaad2
UsamaJaved007 <usama.javed@dubizzlelabs.com>
2024-10-23 23:01:42 +0500

    Updates

---
+++ precache-manifest.58b196675ac122b9293d377360398b05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8513f0a85c5bf06c28c",
+    "url": "/NewsMonkey/static/css/main.43419ed9.chunk.css"
+  },
+  {
+    "revision": "f8513f0a85c5bf06c28c",
+    "url": "/NewsMonkey/static/js/main.5dbcb8be.chunk.js"
+  },
+  {
+    "revision": "75e28b5b988fe68a865d",
+    "url": "/NewsMonkey/static/js/runtime~main.0f92303a.js"
+  },
+  {
+    "revision": "e104d65bea54072cc3d3",
+    "url": "/NewsMonkey/static/js/2.d2caa83d.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/NewsMonkey/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "973434157b03ea6d200bc905c6627da7",
+    "url": "/NewsMonkey/static/media/spinner.97343415.gif"
+  },
+  {
+    "revision": "11ac4730e8914f3a6d567034df04906f",
+    "url": "/NewsMonkey/index.html"
+  }
+];
